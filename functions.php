84c241e854b907bf5acfb00d3e00c10f2582d41f
Yves-X <Yves-X@foxmail.com>
2016-02-27 18:58:57 +0800

    Add theme option for billboard picture's URL

--- functions.php
+++ functions.php
@@ -1,38 +1,43 @@
 <?php
 
 function themeConfig($form) {
-    $slogan = new Typecho_Widget_Helper_Form_Element_Text('slogan', NULL, NULL, _t('首页图片标语文字'), _t('在这里文字，用于在首页中图片的文字显示'));
-    $form->addInput($slogan);
+	$billboard = new Typecho_Widget_Helper_Form_Element_Text('billboard', NULL, NULL, _t('首页图片'), _t('在这里填入一个图片URL, 作为首页图片'));
+	$form->addInput($billboard);
 
-    $siteIcon = new Typecho_Widget_Helper_Form_Element_Text('siteIcon', NULL, NULL, _t('标题栏和书签栏Icon'), _t('在这里填入一个图片URL地址, 作为标题栏和书签栏Icon, 默认不显示'));
-    $form->addInput($siteIcon);
+	$slogan = new Typecho_Widget_Helper_Form_Element_Text('slogan', NULL, NULL, _t('首页图片标语'), _t('在这里填入一段文字，作为首页图片中的标语文字'));
+	$form->addInput($slogan);
 
-    $miibeian = new Typecho_Widget_Helper_Form_Element_Text('miibeian', NULL, _t('粤ICP备14072384号-2'), _t('备案号'), _t('在这里填入天朝备案号，不显示则留空'));
-    $form->addInput($miibeian);
+	$siteIcon = new Typecho_Widget_Helper_Form_Element_Text('siteIcon', NULL, NULL, _t('标题栏和书签栏 Icon'), _t('在这里填入一个图片URL, 作为标题栏和书签栏 Icon, 默认不显示'));
+	$form->addInput($siteIcon);
 
-    $misc = new Typecho_Widget_Helper_Form_Element_Checkbox('misc', array(
-        'ShowLogin' => _t('前台显示登录入口'),
-        'ShowLoadTime' => _t('页脚显示加载耗时')
-        ),
-    array('ShowLogin'), _t('杂项'));
-    $form->addInput($misc->multiMode());
+	$miibeian = new Typecho_Widget_Helper_Form_Element_Text('miibeian', NULL, _t('粤ICP备14072384号-2'), _t('备案号'), _t('在这里填入天朝备案号，留空则不显示'));
+	$form->addInput($miibeian);
+
+	$misc = new Typecho_Widget_Helper_Form_Element_Checkbox('misc', array(
+		'ShowLogin' => _t('前台显示登录入口'),
+		'ShowLoadTime' => _t('页脚显示加载耗时'),
+	),
+		array('ShowLogin'), _t('杂项'));
+	$form->addInput($misc->multiMode());
 }
 
 function timer_start() {
-    global $timestart;
-    $mtime = explode( ' ', microtime() );
-    $timestart = $mtime[1] + $mtime[0];
-    return true;
+	global $timestart;
+	$mtime = explode(' ', microtime());
+	$timestart = $mtime[1] + $mtime[0];
+	return true;
 }
 timer_start();
- 
-function timer_stop( $display = 0, $precision = 3 ) {
-    global $timestart, $timeend;
-    $mtime = explode( ' ', microtime() );
-    $timeend = $mtime[1] + $mtime[0];
-    $timetotal = $timeend - $timestart;
-    $r = number_format( $timetotal, $precision );
-    if ( $display )
-    echo $r;
-    return $r;
+
+function timer_stop($display = 0, $precision = 3) {
+	global $timestart, $timeend;
+	$mtime = explode(' ', microtime());
+	$timeend = $mtime[1] + $mtime[0];
+	$timetotal = $timeend - $timestart;
+	$r = number_format($timetotal, $precision);
+	if ($display) {
+		echo $r;
+	}
+
+	return $r;
 }
--- index.php
+++ index.php
@@ -16,34 +16,35 @@ $this->need('header.php');
 		<div class="row">
 			<div class="col-md-7">
 				<div class="intro animate fadeIn">
-					<h1><?php $this->options->slogan() ?></h1>
+					<h1><?php $this->options->slogan()?></h1>
 					<p class="lead"></p>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
+<style>.billboard { background: #b8d9fa url(<?php $this->options->billboard()?>) 50% 0 repeat-x; background-size: cover; }</style>
 <div class="container">
 	<div class="row">
 
 		<div class="col-md-9">
-		    <?php while($this->next()): ?>
+		    <?php while ($this->next()): ?>
 		    <div class="panel panel-default">
 			    <div class="panel-body">
-			        <h3 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h3>
+			        <h3 class="post-title"><a href="<?php $this->permalink()?>"><?php $this->title()?></a></h3>
 			        <div class="post-meta">
-			        	<span>作者：<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a> | </span>
-			        	<span>时间：<?php $this->date('F j, Y'); ?> | </span>
-			        	<span>分类：<?php $this->category(','); ?> | </span>
-			        	<span>评论：<a href="<?php $this->permalink() ?>"><?php $this->commentsNum('%d 评论'); ?></a> </span>
+			        	<span>作者：<a href="<?php $this->author->permalink();?>"><?php $this->author();?></a> | </span>
+			        	<span>时间：<?php $this->date('F j, Y');?> | </span>
+			        	<span>分类：<?php $this->category(',');?> | </span>
+			        	<span>评论：<a href="<?php $this->permalink()?>"><?php $this->commentsNum('%d 评论');?></a> </span>
 			        </div>
-			        <div class="post-content"><?php $this->content('Continue Reading...'); ?></div>
+			        <div class="post-content"><?php $this->content('Continue Reading...');?></div>
 			    </div>
 		    </div>
-		    <?php endwhile; ?>
-		    <?php $this->pageNav('<< 上一页', '下一页 >>'); ?>
+		    <?php endwhile;?>
+		    <?php $this->pageNav('<< 上一页', '下一页 >>');?>
 		</div>
 
-	<?php $this->need('sidebar.php'); ?>
-	<?php $this->need('footer.php'); ?>
+	<?php $this->need('sidebar.php');?>
+	<?php $this->need('footer.php');?>
 
